010f82e2115818c5e6bf9f4b8e3b5fd65f10de01
1w1ng <921754984@qq.com>
2018-05-22 21:19:50 +0800

    Vuex实现数据共享

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -5,14 +5,19 @@
         <div class="title border-topbottom">当前城市</div>
         <div class="button-list">
           <div class="button-wrapper">
-            <div class="button">深圳</div>
+            <div class="button">{{this.$store.state.city}}</div>
           </div>
         </div>
       </div>
       <div class="area">
         <div class="title border-topbottom">热门城市</div>
         <div class="button-list">
-          <div class="button-wrapper" v-for="item of hot" :key="item.id">
+          <div
+            class="button-wrapper"
+            v-for="item of hot"
+            :key="item.id"
+            @click="handleCityClick(item.name)"
+          >
             <div class="button">{{item.name}}</div>
           </div>
         </div>
@@ -24,9 +29,11 @@
       >
         <div class="title border-topbottom">{{key}}</div>
         <div class="item-list">
-          <div class="item border-bottom"
+          <div 
+            class="item border-bottom"
             v-for="innerItem of item"
             :key="innerItem.id"
+            @click="handleCityClick(innerItem.name)"
           >
             {{innerItem.name}}
           </div>
@@ -45,8 +52,12 @@ export default{
     cities: Object,
     letter: String
   },
-  mounted(){
-    this.scroll = new Bscroll(this.$refs.wrapper)
+  methods: {
+    handleCityClick(city){
+      this.$store.commit('changeCity',city)
+      //路由跳转到首页
+      this.$router.push('/')
+    }
   },
   watch:{
     letter(){
@@ -55,6 +66,9 @@ export default{
         this.scroll.scrollToElement(element)
       }
     }
+  },
+  mounted(){
+    this.scroll = new Bscroll(this.$refs.wrapper)
   }
 }
 </script>
