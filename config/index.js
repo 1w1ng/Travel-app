f5834ae8af524225bc73db378a8e99feb45c797e
1w1ng <921754984@qq.com>
2018-07-11 13:16:28 +0800

    添加预览地址

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/Travel-app/dist/',
 
     /**
      * Source Maps
